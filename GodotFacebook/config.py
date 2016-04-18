f98e683be2ae28083653bc4ffb90bbd5afbb2d04
Shin-NiL <niltonr2000@yahoo.com.br>
2016-04-18 15:22:35 -0300

    Fixed FB SDK Version to 4.10.0

--- GodotFacebook/config.py
+++ GodotFacebook/config.py
@@ -3,7 +3,7 @@ def can_build(plat):
 
 def configure(env):
 	if (env['platform'] == 'android'):
-		env.android_add_dependency("compile 'com.facebook.android:facebook-android-sdk:4.+'")
+		env.android_add_dependency("compile 'com.facebook.android:facebook-android-sdk:4.10.0'")
 		env.android_add_to_manifest("android/AndroidManifestChunk.xml")
 		env.android_add_java_dir("android/src/")
 		env.disable_module()

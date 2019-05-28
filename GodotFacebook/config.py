6deb4fd54fdf78be27786e1efa5de13fbdf4132f
Vasiliy Makarov <drmoriarty.0@gmail.com>
2019-05-28 14:02:18 +0300

    update FB SDK to version 5.0.1

--- GodotFacebook/config.py
+++ GodotFacebook/config.py
@@ -9,7 +9,7 @@ def configure(env):
     if (env['platform'] == 'android'):
         env.android_add_maven_repository("url 'https://maven.google.com'")
         #env.android_add_default_config("manifestPlaceholders = [FACEBOOK_APP_ID: '843075892565403']")
-        env.android_add_dependency("compile 'com.facebook.android:facebook-android-sdk:4.33.+'")
+        env.android_add_dependency("compile 'com.facebook.android:facebook-android-sdk:5.0.+'")
         env.android_add_to_manifest("android/AndroidManifestChunk.xml")
         env.android_add_java_dir("android/src/")
         env.android_add_res_dir("android/res/")
